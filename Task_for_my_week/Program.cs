81d85b2c227852f0fc84be1fa42092781b383506
Mihail <141757584+Mihail544@users.noreply.github.com>
2024-06-19 10:43:43 +0300

    test

---
+++ Task_for_my_week/Program.cs
@@ -0,0 +1,14 @@
+﻿// Odd or Even
+
+Console.Write("Give me your number: ");
+
+int number = int.Parse(Console.ReadLine());
+
+
+if (number % 2  == 0) 
+    
+    {Console.WriteLine("Your number is Even");}
+
+else 
+
+    {Console.WriteLine("Your number is Odd");}
